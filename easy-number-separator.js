3c824a0b46683402a6918f59d15ab161954a51cb
Amir Salehi <nik.amirsa@gmail.com>
2021-09-21 16:14:14 +0430

    add dot support for decimal inputs #1

--- easy-number-separator.js
+++ easy-number-separator.js
@@ -1,11 +1,45 @@
-document.querySelectorAll('.number-separator').forEach((item) => {
-  item.addEventListener('input', (e) => {
-    if (/^[0-9.,]+$/.test(e.target.value)) {
-      e.target.value = parseFloat(
-        e.target.value.replace(/,/g, '')
-      ).toLocaleString('en');
+// Currency Separator
+let commaCounter = 10;
+
+function numberSeparator(Number) {
+  Number += "";
+
+  for (let i = 0; i < commaCounter; i++) {
+    Number = Number.replace(",", "");
+  }
+
+  x = Number.split(".");
+  y = x[0];
+  z = x.length > 1 ? "." + x[1] : "";
+  let rgx = /(\d+)(\d{3})/;
+
+  while (rgx.test(y)) {
+    y = y.replace(rgx, "$1" + "," + "$2");
+  }
+  commaCounter++;
+  return y + z;
+}
+
+const checker = (e) => {
+  if (/^-?\d*[,.]?(\d{0,3},)*(\d{3},)?\d{0,3}$/.test(e.key)) {
+    el.addEventListener("input", function () {
+      e.target.value = numberSeparator(e.target.value);
+    });
+  } else {
+    e.preventDefault();
+    return false;
+  }
+};
+
+document.querySelectorAll(".number-separator").forEach(function (el) {
+  el.addEventListener("keypress", function (e) {
+    if (/^-?\d*[,.]?(\d{0,3},)*(\d{3},)?\d{0,3}$/.test(e.key)) {
+      el.addEventListener("input", function () {
+        e.target.value = numberSeparator(e.target.value);
+      });
     } else {
-      e.target.value = e.target.value.substring(0, e.target.value.length - 1);
+      e.preventDefault();
+      return false;
     }
   });
 });
