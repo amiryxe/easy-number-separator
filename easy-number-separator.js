d54f909bbbdc717c97ad8bdc2aa501159f4c581f
Amir Salehi <nik.amirsa@gmail.com>
2021-09-21 17:06:04 +0430

    dynamically set selector and separator #2

--- easy-number-separator.js
+++ easy-number-separator.js
@@ -1,45 +1,45 @@
-// Currency Separator
-let commaCounter = 10;
+function easyNumberSeparator(config) {
+  // Currency Separator
+  let commaCounter = 10;
 
-function numberSeparator(Number) {
-  Number += "";
+  const obj = config
+    ? config
+    : {
+        selector: ".number-separator",
+        separator: ",",
+      };
 
-  for (let i = 0; i < commaCounter; i++) {
-    Number = Number.replace(",", "");
-  }
-
-  x = Number.split(".");
-  y = x[0];
-  z = x.length > 1 ? "." + x[1] : "";
-  let rgx = /(\d+)(\d{3})/;
-
-  while (rgx.test(y)) {
-    y = y.replace(rgx, "$1" + "," + "$2");
-  }
-  commaCounter++;
-  return y + z;
-}
-
-const checker = (e) => {
-  if (/^-?\d*[,.]?(\d{0,3},)*(\d{3},)?\d{0,3}$/.test(e.key)) {
-    el.addEventListener("input", function () {
-      e.target.value = numberSeparator(e.target.value);
-    });
-  } else {
-    e.preventDefault();
-    return false;
-  }
-};
-
-document.querySelectorAll(".number-separator").forEach(function (el) {
-  el.addEventListener("keypress", function (e) {
-    if (/^-?\d*[,.]?(\d{0,3},)*(\d{3},)?\d{0,3}$/.test(e.key)) {
-      el.addEventListener("input", function () {
-        e.target.value = numberSeparator(e.target.value);
-      });
-    } else {
-      e.preventDefault();
-      return false;
+  function numberSeparator(num) {
+    for (let i = 0; i < commaCounter; i++) {
+      num = num.replace(obj.separator, "");
     }
+
+    x = num.split(".");
+    y = x[0];
+    z = x.length > 1 ? "." + x[1] : "";
+    let rgx = /(\d+)(\d{3})/;
+
+    while (rgx.test(y)) {
+      y = y.replace(rgx, "$1" + obj.separator + "$2");
+    }
+    commaCounter++;
+    return y + z;
+  }
+
+  document.querySelectorAll(obj.selector).forEach(function (el) {
+    el.addEventListener("keypress", function (e) {
+      const reg = new RegExp(
+        `^-?\\d*[${obj.separator}.]?(\\d{0,3}${obj.separator})*(\\d{3}${obj.separator})?\\d{0,3}$`
+      );
+
+      if (reg.test(e.key)) {
+        el.addEventListener("input", function () {
+          e.target.value = numberSeparator(e.target.value);
+        });
+      } else {
+        e.preventDefault();
+        return false;
+      }
+    });
   });
-});
+}
